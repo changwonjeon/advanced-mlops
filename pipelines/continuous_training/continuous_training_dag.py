330eac0c4433f412c63db854cad07bdcb4b560e5
Jeon Changwon <9064086+changwonjeon@users.noreply.github.com>
2025-10-16 11:08:31 +0900

    2025-10-16_모델학습직전까지_오전11시

--- pipelines/continuous_training/continuous_training_dag.py
+++ pipelines/continuous_training/continuous_training_dag.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import pendulum
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.sdk import DAG, Variable
 
 from utils.callbacks import failure_callback, success_callback
@@ -25,7 +24,6 @@ kst_ds_template = (
     "{{ data_interval_start.in_timezone('Asia/Seoul').to_date_string() }}"
 )
 
-
 with DAG(
     dag_id="credit_score_classification_ct",
     default_args={
@@ -41,13 +39,37 @@ with DAG(
     catchup=False,
     tags=set(["lgcns", "mlops"]),
 ) as dag:
-    # TODO: 코드 작성
-    # 아래 Task를 적절한 Operator를 사용하여 구현
+    data_extract = SQLExecuteQueryOperator(
+        task_id="data_extraction",
+        conn_id=conn_id,
+        sql=read_sql_file(sql_file_path),
+        split_statements=True,
+    )
 
-    data_extract = EmptyOperator(task_id="data_extraction")
+    data_preprocessing = BashOperator(
+        task_id="data_preprocessing",
+        bash_command=f"cd {airflow_dags_path}/pipelines/continuous_training/docker &&"
+        "docker compose up --build && docker compose down",
+        env={
+            "PYTHON_FILE": "/home/codespace/data_preprocessing/preprocessor.py",
+            "MODEL_NAME": "credit_score_classification",
+            "BASE_DT": kst_ds_template,
+        },
+        append_env=True,
+        retries=1,
+    )
 
-    data_preprocessing = EmptyOperator(task_id="data_preprocessing")
-
-    training = EmptyOperator(task_id="model_training")
+    training = BashOperator(
+        task_id="model_training",
+        bash_command=f"cd {airflow_dags_path}/pipelines/continuous_training/docker &&"
+        "docker compose up --build && docker compose down",
+        env={
+            "PYTHON_FILE": "/home/codespace/training/trainer.py",
+            "MODEL_NAME": "credit_score_classification",
+            "BASE_DT": kst_ds_template,
+        },
+        append_env=True,
+        retries=1,
+    )
 
     data_extract >> data_preprocessing >> training
