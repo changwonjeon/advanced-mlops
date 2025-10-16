32b949e4bb8326fa47980e3366be86c99056736b
Jeon Changwon <9064086+changwonjeon@users.noreply.github.com>
2025-10-16 16:41:51 +0900

    2025-10-16, CD까지 완료.최종끝

--- pipelines/continuous_deployment/continuous_deployment_dag.py
+++ pipelines/continuous_deployment/continuous_deployment_dag.py
@@ -13,6 +13,8 @@ from airflow.sdk import DAG, Variable, get_current_context
 
 from utils.callbacks import failure_callback, success_callback
 
+from fastapi import status
+
 local_timezone = pendulum.timezone("Asia/Seoul")
 airflow_dags_path = Variable.get("AIRFLOW_DAGS_PATH")
 
@@ -24,10 +26,13 @@ def get_branch_by_api_status() -> list[str] | str:
     """
     try:
         response = requests.get("http://localhost:3000/healthz")
-        if response.status_code == 200:
+        if response.status_code == status.HTTP_200_OK: #200값 -> fastapi에 status으로 정의된... 이해하기 쉽다.. 숫자만으론 어려워~
             # TODO: 헬스체크의 응답이 올바르게 왔다면 다음 Task를 실행해야 함
             # "get_deployed_model_creation_time", "get_latest_trained_model_creation_time" 를 실행해야 함
-            return
+            return [
+                "get_deployed_model_creation_time",
+                "get_latest_trained_model_creation_time",
+            ]
         else:
             return "deploy_new_model"
     except Exception as e:
@@ -39,9 +44,11 @@ def get_deployed_model_creation_time() -> datetime | None:
     """이미 배포된 모델의 `creation_time`을 조회합니다."""
     try:
         response = requests.post("http://localhost:3000/metadata")
-        if response.status_code == 200:
+        if response.status_code == status.HTTP_200_OK: #200값 -> fastapi에 status으로 정의된... 이해하기 쉽다.. 숫자만으론 어려워~
             # TODO: 메타데이터 조회 응답이 올바르게 왔다면 메타데이터 내 모델의 생성 시간(creation_time)을 datetime 객체로 반환해야 함
-            return
+            return datetime.strptime(
+                response.json().get("creation_time"), "%Y-%m-%dT%H:%M:%S.%fZ" # response.json() -> dictionary이므로 .get()으로, 그다음 형식을 datetime.strptime으로 파싱
+            )
         else:
             print(
                 f"`creation_time`을 불러올 수 없습니다.: {response.status_code}"
@@ -57,7 +64,7 @@ def get_latest_trained_model_creation_time() -> datetime | None:
     try:
         bento_model = bentoml.models.get("credit_score_classification:latest")
         # TODO: bento_model의 creation_time의 timezone 정보를 제거하고 반환
-        return
+        return bento_model.info.creation_time.replace(tzinfo=None) #timezone 정보 날리기...
     except Exception as e:
         print(f"Error getting latest trained model creation time: {e}")
         return None
@@ -95,7 +102,7 @@ def decide_model_update():
     if (
         trained_creation_time is not None
         and trained_creation_time > deployed_creation_time
-    ):
+    ): ## 그냥 elif으로 train..time > de...time 만 비교하면 되지 않나?
         print("Deployed model is already out-of-date.")
         return "deploy_new_model"
 
@@ -119,29 +126,51 @@ with DAG(
     tags=set(["lgcns", "mlops"]),
 ) as dag:
     # TODO: API 상태 체크 결과 가져오기
-    get_api_status_task = EmptyOperator(task_id="get_branch_by_api_status")
+    # get_api_status_task = EmptyOperator(task_id="get_branch_by_api_status")
+    get_api_status_task = BranchPythonOperator(
+        task_id="get_branch_by_api_status",
+        python_callable=get_branch_by_api_status,
+    ) 
 
     # TODO: 현재 컨테이너에서 실행 중인 모델의 creation_time 가져오기
-    get_deployed_model_creation_time_task = EmptyOperator(
+    # get_deployed_model_creation_time_task = EmptyOperator(
+    #     task_id="get_deployed_model_creation_time",
+    # )
+    get_deployed_model_creation_time_task = PythonOperator(
         task_id="get_deployed_model_creation_time",
+        python_callable=get_deployed_model_creation_time,
     )
 
     # TODO: 로컬에서 최신 학습된 모델의 creation_time 가져오기
-    get_latest_trained_model_creation_time_task = EmptyOperator(
+    # get_latest_trained_model_creation_time_task = EmptyOperator(
+    #     task_id="get_latest_trained_model_creation_time",
+    # )
+    get_latest_trained_model_creation_time_task = PythonOperator(
         task_id="get_latest_trained_model_creation_time",
+        python_callable=get_latest_trained_model_creation_time,
     )
 
     # TODO: 모델을 업데이트할지 결정
-    decide_update_task = EmptyOperator(
+    # decide_update_task = EmptyOperator(
+    #     task_id="decide_update",
+    # )
+    decide_update_task = BranchPythonOperator(
         task_id="decide_update",
+        python_callable=decide_model_update,
     )
 
     # TODO: 새로운 모델을 배포
-    deploy_new_model_task = EmptyOperator(
+    # deploy_new_model_task = EmptyOperator(
+    #     task_id="deploy_new_model",
+    # )
+    deploy_new_model_task = BashOperator(
         task_id="deploy_new_model",
+        bash_command=f"cd {airflow_dags_path}/api/docker &&"
+        "docker compose up --build --detach",
+        trigger_rule="one_success", # dag에서 skip되는것 방지
     )
 
-    # 배포를 건너뛸 경우 실행할 더미 태스크
+    # 배포를 건너뛸 경우 실행할 더미 태스크 -> EmptryOperator도 가능
     skip_deployment_task = PythonOperator(
         task_id="skip_deployment",
         python_callable=lambda: print("No new model to deploy"),
